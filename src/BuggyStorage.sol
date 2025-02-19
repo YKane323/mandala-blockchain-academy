289d6da09c93db4563e22187470ed51e47a32848
YKane323 <dharma15yudha@gmail.com>
2025-02-19 22:46:30 +0800

    Homework 2 : Yudha
    i have done all task and bug on the home work 2

--- src/BuggyStorage.sol
+++ src/BuggyStorage.sol
@@ -4,44 +4,66 @@ pragma solidity ^0.8.0;
 contract BuggyStorage {
     // DO NOT EDIT COMMENTS
     // Task 1: Declare an array as numbers;
+    uint256[] public numbers;
     // Task 2: Declare a string as message;
+    string public message;
     // Task 3: Declare a mapping of address to balance as balances;
+    mapping (address => uint256) public balances;
 
-    // Bug 1: Array bounds
-    function getNumber(uint index) public view returns (uint) {
+    //Bug 1: Array bounds
+    function getNumber(uint256 index) public view returns (uint256) {
+        require(index < numbers.length , "please input index again , your input is out of range");
         return numbers[index]; 
     }
 
-    function addNumber(uint number) public {
+    function addNumber(uint256 number) public {
         // Task 4: Implement adding numbers to the array
+        numbers.push(number);
     }
 
     // Bug 2: Slice array correctly
-    function sliceArray(uint start, uint end) public view returns (uint[] memory) {
-        return numbers[start:end];
+    function sliceArray(uint256 start, uint256 end) public view returns (uint256[] memory) {
+        require(start < end , "data start must be lesser then end");
+        require(end <= numbers.length , "end input must be lesser then array length");
+
+        uint256[] memory _sliceArray = new uint256[](end - start);
+
+        for (uint256 i = start ; i <  end ;  i++){
+            _sliceArray [i - start] = numbers [i];
+        }
+
+        return _sliceArray;
     }
 
     // Bug 3: Concatenate strings correctly
-    function setMessage(string memory newMessage) public {
-        message += newMessage;
+    function setMessage(string memory newMessage) public view returns (string memory) {
+       return string.concat(message, newMessage);
     }
 
     // Bug 4: How do we update numbers with memory as params?
-    function validMemoryUsage(uint[] memory input) public {
-        uint[] storage ref = input;
+    function validMemoryUsage(uint256[] memory input) public {
         // Implement logics to update numbers here
+        require(input.length > 0 , " please input a valid number");
+
+        for (uint256 i = 0 ; i < input.length ; i++){
+            numbers.push(input[i]);
+        }
+
     }
 
-    function getBalance(address user) public view returns (uint) {
+    function getBalance(address user) public view returns (uint256) {
         // Task 5: Returning the balance for a specific user
+        return balances[user];
     }
 
     // Bug 5: How to safely pop elements from the array?
     function removeLastNumber() public {
+        require(numbers.length > 0 , "the array is empty");
         numbers.pop();
     }
 
-    function updateBalance(address user, uint amount) public {
+    function updateBalance(address user, uint256 amount) public {
         // Task 6: Update balance of a user with a specific amount
+        balances [user] = amount;
     }
 }
